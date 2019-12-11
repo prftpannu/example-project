4e972458acdfb7f0e458c52dd2485a94e6c62c9b
prftpannu <48219087+prftpannu@users.noreply.github.com>
2019-12-11 15:08:42 -0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,9 @@
+import java.text.DateFormat; 
+import java.text.SimpleDateFormat;  
+import java.util.Date; 
+public class test{
+public static void main(String[] args)
+{
+DateFormat currentDateFormat = new SimpleDateFormat("dd MMM yyyy"); 
+}
+}
